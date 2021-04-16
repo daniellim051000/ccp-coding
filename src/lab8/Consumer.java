1c0d38427dab83d44990c44d358977a8a0c90b54
Daniel Lim Zheng Yu <daniellim0510@Daniel-Lim-MacAir.local>
2021-04-16 11:40:37 +0800

    lab8 coding

---
+++ src/lab8/Consumer.java
@@ -0,0 +1,18 @@
+/********************CONSUMER*******************************/
+
+class Consumer implements Runnable {
+
+    Buffer<Character> buf;
+
+    Consumer(Buffer<Character> b) {buf = b;}
+
+    public void run() {
+      try {
+        while(true) {
+            ThreadPanel.rotate(180);
+            Character c = buf.get();
+            ThreadPanel.rotate(180);
+        }
+      } catch(InterruptedException e ){}
+    }
+}
